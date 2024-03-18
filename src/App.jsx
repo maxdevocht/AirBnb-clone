7df5f33657447b956a13241669bc85efe785ed4a
Max de Vocht <maxdevocht@hotmail.com>
2024-03-18 13:09:10 +0100

    Update props
    Added Math numbers

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ export default function App() {
       <Card
         img="./images/katie-zaferes.png"
         rating="5.0"
-        reviewCount="(6)"
+        reviewCount={6}
         country="USA"
         title="Life Lessons with Katie Zaferes"
-        price="136"
+        price={136}
       />
     </div>
   )
