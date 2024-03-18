ada8e5ee2e531adad8695b47625bf189ed2cdaf6
Max de Vocht <maxdevocht@hotmail.com>
2024-03-18 16:25:11 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,7 @@ export default function App() {
     return (
       <Card 
         key={item.id}
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
-        openSpots={item.openSpots}
+        item={item}
       />
     )
   })
